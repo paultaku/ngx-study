9b64b394f734ebbb561eb2cd4441fbc94f9dc85b
Ku Hung-Hsi <justlove0714@hotmail.com>
2018-11-28 08:45:41 +0800

    scrolling cdk study

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, ChangeDetectionStrategy, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { ScrollDispatcher, CdkScrollable } from '@angular/cdk/scrolling';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  @ViewChild(CdkScrollable) scrollable: CdkScrollable;
   title = 'app';
+  items = Array.from({length: 50}).map((_, i) => `Item #${i}`);
+  constructor(private scrollDispatcher: ScrollDispatcher) {
+  }
+
+  ngOnInit() {
+  }
+
+  scrollDown() {
+    console.log(this.scrollable.measureScrollOffset('top'));
+    const top = this.scrollable.measureScrollOffset('top');
+    this.scrollable.scrollTo({
+      top: top + 200
+    });
+  }
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { ScrollDispatchModule } from '@angular/cdk/scrolling';
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -8,7 +8,8 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ScrollDispatchModule
   ],
   providers: [],
   bootstrap: [AppComponent]
